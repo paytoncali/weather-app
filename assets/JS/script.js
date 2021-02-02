dac62883088bb7d58ed1ceb68c8480858d3f71c6
Payton Whinnery <paytoncaliw@gmail.com>
2021-02-01 19:24:48 -0600

    current weather box is working and functional. adding cards for 5day weather forecast

--- assets/JS/script.js
+++ assets/JS/script.js
@@ -15,10 +15,17 @@ function currentApi (event) {
         var humidity = document.querySelector(".humidity");
         var windSpeed = document.querySelector(".windspeed");
         var cityName = document.querySelector(".city");
+        var iconEl = document.createElement("img");
+        var weatherIconEl = document.querySelector(".weathericon");
+
         var lat = data.coord.lat;
         var lon = data.coord.lon;
+        console.log(lat);
+        console.log(lon);
 
-        cityName.textContent = data.name + data.weather[0].icon;
+        iconEl.setAttribute("src", "http://openweathermap.org/img/wn/" + data.weather[0].icon + "@2x.png");
+        weatherIconEl.append(iconEl);
+        cityName.textContent = data.name;
         temp.textContent = "Temperature: " + data.main.temp + "°F";
         humidity.textContent = "Humidity: " + data.main.humidity + "%";
         windSpeed.textContent = "Wind Speed: " + data.wind.speed + " MPH";
@@ -30,17 +37,19 @@ function currentApi (event) {
 
 function currentUV (lat, lon) {
 
-    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,daily,alerts&appid=73764a83501606e04b74a8e0281a654b")
+    fetch("https://api.openweathermap.org/data/2.5/onecall?lat=" + lat + "&lon=" + lon + "&exclude=minutely,hourly,daily,alerts&units=imperial&appid=73764a83501606e04b74a8e0281a654b")
     .then(function (response) {
         console.log(response);
         return response.json();
     })
-    .then(function (data){
+    .then(function (data) {
         console.log(data);
+        var uvIndex = document.querySelector(".uv");
+        uvIndex.textContent = "UV Index " + data.current.uvi;
+        console.log(data.current.uvi);
+        // if statement here
     })
-    var uvIndex = document.querySelector(".uv");
 
-    uvIndex.textContent = "UV Index " + data.current.uvi;
 }
 
 
